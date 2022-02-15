e4a0cec2b93bf1ec2bfeef8458a4c97d51a0c277
agungor34 <akifgungor@gmail.com>
2022-02-15 10:15:29 +0100

    4rd commit

--- aws/projects/Apache-Nginx-Flask/apngfl_startbatch.sh
+++ aws/projects/Apache-Nginx-Flask/apngfl_startbatch.sh
@@ -19,6 +19,23 @@ amazon-linux-extras install nginx1 -y
 chmod -R 777 /usr/share/nginx/html
 rm index.html
 cp /home/ec2-user/my-projects/aws/projects/Apache-Nginx-Flask/cikolata/* /usr/share/nginx/html/
+# get private ip address of ec2 instance using instance metadata
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` \
+&& PRIVATE_IP=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4`
+# get public ip address of ec2 instance using instance metadata
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` \
+&& PUBLIC_IP=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4` 
+# get date and time of server
+DATE_TIME=`date`
+# create a custom index.html file
+echo "<body>
+    <h1>Testing Application Load Balancer and Autoscaling</h1>
+    <p>This web server is launched from launch template by Clarusway</p>
+    <p>This instance is created at <b>$DATE_TIME</b></p>
+    <p>Private IP address of this instance is <b>$PRIVATE_IP</b></p>
+    <p>Public IP address of this instance is <b>$PUBLIC_IP</b></p>
+</body>
+</html>" >> /usr/share/nginx/html/index.html
 my_ip=`curl http://checkip.amazonaws.com`
-sed -i "s/link11/$my_ip/g" /usr/share/nginx/html/index.html
+sed -i "s/link11/$PUBLIC_IP/g" /usr/share/nginx/html/index.html
 systemctl start nginx
--- python/python_myself/python_variables.py
+++ python/python_myself/python_variables.py
@@ -1,5 +1,5 @@
 
-
+"""
 string1="Mehmet 1994 yılında doğdu"
 print(string1, end=" ")
 # "print" fonksiyonun "end" parametresiyle kullanılması
@@ -17,14 +17,14 @@ print(tuple2[4])
 List_1=[4,3,5.25,"Ayşe",'a',[2,6,4.5]]
 print(List_1[1])
 
-""" Dictionary: Sözlükler süslü parantezler ile ifade edilir ve  iki kısımdan oluşur; keys(anahtar) ve value(değer),  value kısmı bütün veri türünü içerebilir fakat keys kısmı sadece string ve int tipinde olabilir."""
+
+#Dictionary: Sözlükler süslü parantezler ile ifade edilir ve  iki kısımdan oluşur; keys(anahtar) ve value(değer),  value kısmı bütün veri türünü içerebilir fakat keys kısmı sadece string ve int tipinde olabilir.
 
 Dict_iller ={1:"Ankara",2:"İstanbul",3:"İzmir"}
+print(type(Dict_iller))
 
-"""
 isim = input("İsminizi Giriniz :")
 print(isim)
-"""
 
 
 # "input" fonksiyonu
@@ -66,11 +66,13 @@ for sayı in range(0,11):
 print()
 
 """
-for harf in "Python": {
+"""
+for harf in "Python":
+
     print(harf, end=" ")
     print()
-}
-"""
+
+
 
 # "len()" fonksiyonu
 metin ="Python"
@@ -83,10 +85,10 @@ parolam ="1234"
  
 giris_hakki=3
 
-"""
-break and continue
-*** break döngüden çıkar
-*** continue döngünün başına gider
+
+#break and continue
+#*** break döngüden çıkar
+#*** continue döngünün başına gider
 
 while giris_hakki>0:
     giris_hakki -=1
@@ -104,5 +106,157 @@ for sayi in range(20):
         continue
     else:
         print(sayi)
+
+
+liste =[2,"Python",5.4,[5,3],("Java",5,'a')]
+
+print("liste",liste)
+print (liste[2])
+print (liste[4][0])
+print (liste[0:2])
+print ("[0:5:2],Liste[başlangıç,bitiş,artış miktarı]",liste[0:5:2])
+
+print ("list'in metodlarını görme:dir(list)",dir(list))
+
+meyveler=['elma', 'armut', 'kiraz', 'karpuz', 'Üzüm', 'portakal', 'armut']
+meyveler.pop()
+
+sayilar ={"1":"bir","2":"iki","3":"üç","4":"dört","5":"beş"}
+ 
+print(sayilar.setdefault("4"),)
+print(sayilar.setdefault("4","sekiz"),)
+
+print(dir(dict))
+
+
+
+#KÜMELER
+
+kume = set()
+kume= {"Python",'c',4,"Cahit"}
+print(kume,type(kume))
+
+kume ={1,2,3,4,5}
+kume.add(6)
+print(kume)
+
+kume ={1,2,3,4,5,6}
+kume_yedek =set()
+kume_yedek=kume.copy()
+print(kume_yedek)
+
+kume ={1,2,3,4,5,6}
+kume_2={7,8,9,10}
+kume.update(kume_2)
+print(kume)
+
+A = {1,2,3,4,9}
+B = {3,4,5,6,10}
+C = {4,7,8,9,10}
+print(type(A),type(B),type(C))
+print(A.union(B.union(C)))
+print(A.intersection(B))
+
+
+import string
+print(dir(string))
+
+metin ="akifgungor.com python calismalari"
+print(metin.capitalize())
+print(metin.title())
+print(metin.count("o"))
+print(metin.upper())
+print(metin.title().swapcase())
+print(metin.center(45))
+print(metin.replace("o","x"))
+print("*".join(metin))
+
+diller=["Python","Java","Ruby","C#"]
+print(diller)
+print(",".join(diller))
+
+diller="Python,Java,Ruby,C#,aou,uoa"
+print(diller.split("o"))
+
+
+# Error, Bug, Exception
+try:
+
+    sayi1 = int(input("Bölünecek Sayıyı :"))
+    sayi2 = int(input("Bölecek Sayıyı :"))
+
+    sonuc = sayi1/sayi2
+    print("Sonuc :",sonuc)
+
+except ZeroDivisionError:
+    print("Bir sayıyı sıfıra bölemezsiniz lütfen sıfır dışında bir sayı girin")
+except ValueError:
+    print("Lütfen sayısal bir karakter girin..")
+except:
+    print("Beklenmeyen bir hata oluştu")
+
+#try:
+ 
+    sayi1 = int(input("Birinci Sayı :"))
+    sayi2 = int(input("İkinci Sayı :"))
+    sonuc = sayi1/sayi2
+    print("Sonuc :",sonuc)
+ 
+#except ZeroDivisionError  as acıklama:
+  #  print(acıklama)
+
+yari_cap = int(input("Yarıçapı girin :"))
+pi=3.14
+if yari_cap <= 0:
+    raise ValueError("Yarıçap sıfır ve sıfırdan küçük bir değer olamaz")
+
+sonuc =pi*(yari_cap**2)
+print("Alan :{}'dir".format(sonuc))
+
+
+#Dosya İşlemleri
+import os
+print(os.getcwd())
+
+dosya = open("deneme.txt","a+")
+dosya.write("Merhaba Millet :)")
+dosya.close()
+
+with open("deneme.txt","a+") as dosya:
+    dosya.write("Merhaba Millet :)")
+
+with open("deneme.txt","a+") as dosya:
+    dosya.writelines(["Merhaba Millet\n","Python Derslerine Hosgeldiniz"])
+
+print()
+
+if os.path.isfile("deneme.txt"):
+  with open("deneme.txt") as dosya:
+    dosya.seek(10)
+    print(dosya.read())
+
+else:
+    print("dosya bulunamadı")
+
+os.remove("deneme.txt")
 """
 
+def yazdir():
+    print("akifgungor.com Python Örnekleri")
+
+yazdir()
+
+def yazdir():
+    return "akifgungor.com Python Dersleri"
+
+print(yazdir())
+
+def toplama(sayi1,sayi2):
+    return sayi1+sayi2
+
+print("Sonuç :",toplama(3,4))
+
+def toplama(sayi1=6,sayi2=8):
+    return sayi1+sayi2x
+
+print("Sonuç :",toplama(4,3))
